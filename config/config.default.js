53da1fe6c0dadd9f1fc93671414c20c1cd8e8557
yogeshVU <yogesh.d.barve@vanderbilt.edu>
2017-12-13 03:40:16 +0000

    updating meta

--- config/config.default.js
+++ config/config.default.js
@@ -7,5 +7,13 @@ var config = require('./config.webgme'),
 config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 
+
+// Seeds
+config.seedProjects.enable = true;
+config.seedProjects.basePaths = ["./seeds"]
+
+// Icons
+config.visualization.svgDirs.push('./icons/png');
+
 validateConfig(config);
 module.exports = config;
