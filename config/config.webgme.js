5390a0ed1ec14f4d44fde48d644379a5cedd4a48
yogeshVU <yogesh.d.barve@vanderbilt.edu>
2017-12-15 17:47:27 +0000

    added the chronograph visualizer

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -10,14 +10,18 @@ var config = require('webgme/config/config.default'),
 
 
 
+config.visualization.panelPaths.push(__dirname + '/../src/visualizers/panels');
 
 
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
-
+config.requirejsPaths = {
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets'
+};
 
 
 config.mongo.uri = 'mongodb://127.0.0.1:27017/fecbenchstudio';
